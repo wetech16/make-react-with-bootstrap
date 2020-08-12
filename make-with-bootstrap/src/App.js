0e94a761cfa9d7319559acc98b284f63da173864
Stephen Guo <wetech16@gmail.com>
2020-08-12 16:45:28 -0700

    -css
    css and img

--- make-with-bootstrap/src/App.js
+++ make-with-bootstrap/src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import './App.css';
-import Html from './Components/Html';
+import Html from './Html';
 
 function App() {
   return (
     <div className="App">
-      <Html />
+      <Html></Html>
     </div>
   );
 }
