88558b2a78557fd1261f4aac9f4316ff02474e56
Stephen Guo <wetech16@gmail.com>
2020-08-12 10:30:00 -0700

    added html
    add index.html to component and edit it to run

---
+++ make-with-bootstrap/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import Html from './Components/Html';
+
+function App() {
+  return (
+    <div className="App">
+      <Html />
+    </div>
+  );
+}
+
+export default App;
